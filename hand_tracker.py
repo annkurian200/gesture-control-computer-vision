ab4d61ca2e3300681268b85215c58376b1e8636b
annkurian200 <annkurian200@gmail.com>
2026-09-16 22:32:19 +0530

    Improved real-time hand tracking and gesture drawing

--- hand_tracker.py
+++ hand_tracker.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+import time
 
 BaseOptions = mp.tasks.BaseOptions
 HandLandmarker = mp.tasks.vision.HandLandmarker
@@ -11,8 +12,8 @@ options = HandLandmarkerOptions(
     base_options=BaseOptions(
         model_asset_path="hand_landmarker.task"
     ),
-    running_mode=VisionRunningMode.IMAGE,
-    num_hands=2
+    running_mode=VisionRunningMode.VIDEO,
+    num_hands=1
 )
 
 with HandLandmarker.create_from_options(options) as landmarker:
@@ -24,6 +25,7 @@ with HandLandmarker.create_from_options(options) as landmarker:
     previous_point = None
     smooth_x = None
     smooth_y = None
+    
     while True:
         success, frame = cap.read()
         if not success:
@@ -39,8 +41,8 @@ with HandLandmarker.create_from_options(options) as landmarker:
         )
 
         # Detect hands
-        result = landmarker.detect(mp_image)
-
+        timestamp_ms = int(time.monotonic() * 1000)
+        result = landmarker.detect_for_video(mp_image, timestamp_ms)
         # Draw hand landmarks
         if result.hand_landmarks:
             for hand in result.hand_landmarks:
@@ -53,8 +55,8 @@ with HandLandmarker.create_from_options(options) as landmarker:
                     smooth_x = target_x
                     smooth_y = target_y
 
-                smooth_x = int(smooth_x * 0.7 + target_x * 0.3)
-                smooth_y = int(smooth_y * 0.7 + target_y * 0.3)
+                smooth_x = int(smooth_x * 0.4 + target_x * 0.6)
+                smooth_y = int(smooth_y * 0.4 + target_y * 0.6)
                 x = smooth_x
                 y = smooth_y
                 cv2.circle(frame, (x, y), 10, (255, 0, 0), -1)
@@ -133,31 +135,13 @@ with HandLandmarker.create_from_options(options) as landmarker:
                     )
 
                     if previous_point is not None:
-
-                        distance_between = np.linalg.norm(
-                            np.array((x, y)) - np.array(previous_point)
-                        )
-
-                        steps = max(1, int(distance_between / 5))
-
-                        for i in range(1, steps + 1):
-                            intermediate_x = int(
-                            previous_point[0] +
-                            (x - previous_point[0]) * i / steps
-                            )
-
-                            intermediate_y = int(
-                            previous_point[1] +
-                            (y - previous_point[1]) * i / steps
-                            )
-
-                            cv2.line(
-                            canvas,
-                            previous_point,
-                            (intermediate_x, intermediate_y),
-                            (255, 255, 255),
-                            5
-                            )
+                        cv2.line(
+        canvas,
+        previous_point,
+        (x, y),
+        (255, 255, 255),
+        5
+    )
 
                     previous_point = (x, y)
 
